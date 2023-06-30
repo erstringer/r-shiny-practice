30d52d1a86f3cf5b430cf8cf2d6e300368996f9e
Emily Rose Stringer <estringer24@coa.edu>
2023-06-30 21:45:10 +0000

    Create ui and server

--- Hello-Shiny-UFO-Sightings/app.R
+++ Hello-Shiny-UFO-Sightings/app.R
@@ -1,30 +1,16 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI ----
 ui <- fluidPage(
-  titlePanel("UFO Sightings"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      h4 ("an extraterrestrial introduction to Rshiny", align = "center")),
-    mainPanel(sliderInput("range", 
-                                 label = "Range of interest:",
-                                 min = 0, max = 15, value = c(0, 15))
-      )))
+   sliderInput(inputId = "num", 
+               label = "choose your number", 
+               value = 55, min = 5, max = 100), 
+   )
 
+server <- function(input, output) {
+  output$hist<- renderPlot({
+    hist(rnorm(input$num))
+  })
+}
 
-# Define server logic ----
-server <- function(input, output) {}
-
-# Run the app ----
 shinyApp(ui = ui, server = server)
 
